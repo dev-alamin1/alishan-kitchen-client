8dd4d87e53f0deada28e3571f59f95cc4d31b9c8
dev-alamin1 <dev.alaminmondal@gmail.com>
2022-11-09 14:51:33 +0600

    recto-photo-view added

--- src/Components/Home/RecipieDescription.js
+++ src/Components/Home/RecipieDescription.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const RecipieDescription = () => {
   const recipe = useLoaderData();
@@ -20,7 +21,15 @@ const RecipieDescription = () => {
       <div className="md:w-2/3 px-10 md:mx-auto py-10 ">
         <div className="card card-compact w-auto bg-base-100 shadow-xl">
           <figure>
-            <img src={photoURL} alt="food-recipies" className="w-full h-96" />
+
+           
+
+             <PhotoProvider>
+                <PhotoView src={photoURL}>
+                    <img src={photoURL} alt="food-recipies" className="w-full h-96" style={{ objectFit: 'cover' }}  />
+                </PhotoView>
+            </PhotoProvider>
+            
           </figure>
           <div className="card-body">
             <h2 className="card-title">{foodName}</h2>
@@ -32,17 +41,17 @@ const RecipieDescription = () => {
                 <h2 className="text-3xl text-amber-500">Ingredients </h2>
 
                 <div className="w-full">
-                  <div class="mt-5 w-full rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5">
+                  <div className="mt-5 w-full rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5">
                     <div
-                      class="py-2 divide-y divide-gray-100"
+                      className="py-2 divide-y divide-gray-100"
                       role="menu"
                       aria-orientation="vertical"
                       aria-labelledby="options-menu"
                     >
 
                         
-                      {ingredients.map((ingredient) => (
-                        <li className="text-2xl ml-2 mr-0">{ingredient}</li>
+                      {ingredients.map((ingredient,index) => (
+                        <li className="text-2xl ml-2 mr-0" key={index}>{ingredient}</li>
                       ))}
                       
                     </div>
@@ -55,16 +64,16 @@ const RecipieDescription = () => {
               <div >
                 <h2 className="text-3xl text-amber-500">Instruction For Cooking </h2>
 
-                <div class="w-full">
-                  <div class="mt-5 w-full rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5">
+                <div className="w-full">
+                  <div className="mt-5 w-full rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5">
                     <div
-                      class="py-2 divide-y divide-gray-100"
+                      className="py-2 divide-y divide-gray-100"
                       role="menu"
                       aria-orientation="vertical"
                       aria-labelledby="options-menu"
                     >
-                      {cookingInstructions.map((ins) => (
-                        <li className="text-2xl ml-2 mr-0 mb-2">{ins}</li>
+                      {cookingInstructions.map((ins,index) => (
+                        <li className="text-2xl ml-2 mr-0 mb-2" key={index}>{ins}</li>
                       ))}
                       
                     </div>
